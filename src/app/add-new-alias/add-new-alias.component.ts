d94905d34604614629d90310b413cb3966e833f5
gxDTsqdXrbKlpBrN <1sh4kg0nu1@gmail.com>
2023-09-24 18:11:17 +0300

    feat: add to localstorage functions

--- src/app/add-new-alias/add-new-alias.component.ts
+++ src/app/add-new-alias/add-new-alias.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-new-alias.component.css']
 })
 export class AddNewAliasComponent {
+  public name:string = "";
+  public text:string = "";
+  saveNewAlias(){
+    if(this.name ==="" || this.text === "" || this.name == undefined || this.text == undefined){
+      alert("You should fill all the inputs!");
+      return;
+    }
 
+    const alias = {
+      name: this.name,
+      text:this.text
+    }
+    let aliases = localStorage.getItem("aliases");
+    if(!aliases){
+      localStorage.setItem("aliases", JSON.stringify([alias]))
+    }else{
+      const aliases_array:Array<any> = JSON.parse(aliases);
+      aliases_array.push(alias);
+      localStorage.setItem("aliases", JSON.stringify(aliases_array));
+      alert(`Saved alias for ${alias.name}`)
+    }
+  }
 }
