3b1ee2e30d1813acf2cab1680f96c7abd3380a35
İshak Gönül <ishak.gonul@titra.com.tr>
2023-10-04 13:25:52 +0300

    chore: dont open devtools on startup

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function createWindow() {
   win.loadFile('./dist/frontend/index.html')
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
