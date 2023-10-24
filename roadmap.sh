d118b371552d188e353b3b565d358edc4bc9b78e
İshak Gönül <76657662+shakg@users.noreply.github.com>
2023-10-24 19:00:45 +0300

    docs: create roadmap.sh

---
+++ roadmap.sh
@@ -0,0 +1,14 @@
+### Short term roadmap 
+- [ ] Add contribution docs.
+- [x] ~Make Export import functions work~
+- [x] ~Test on linux and mac~
+- [x] ~Edit aliases~
+- [x] ~Write types in angular side~
+- [x] ~Add backend frontend communication~
+- [ ] Add central backend to share across teams and/or other users
+- [ ] add sign in / auth
+
+### Mid / long term roadmap
+- [ ] Publish as a browser plugin to use it like a proper text expander
+- [ ] Add description to aliases
+- [ ] Add LLM to ask question like `how to find Q2 performance excel?` and find relevant links.
